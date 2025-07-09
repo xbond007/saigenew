bdf99455413622c6d8d13c9f93681afa0f6a7adf
xbond007 <144120275+xbond007@users.noreply.github.com>
2025-07-09 13:10:27 +0800

    Rewrite user code and main

--- user/common.h
+++ user/common.h
@@ -1,53 +1,37 @@
 #ifndef COMMON_H
 #define COMMON_H
+
 #include "cw32f003.h"
-#include "stdbool.h"
-#include "SEGGER_RTT.h"
 #include "cw32f003_gpio.h"
-#include "type.h"
-typedef enum {
-    lock = 1,
-    unlock,
-} LOCK_T;
-typedef struct
-{
-    uint8_t filename[20];
-    uint8_t filelenbuf[10];
-    uint32_t FileLen;
-    uint8_t transmission_start;
-    uint8_t end_flag;
-    uint32_t offset;
-} FILE_T;
+#define LIGHT_PORT      CW_GPIOC
+#define LIGHT_PIN       GPIO_PIN_0
 
-// 灯光输出检测e
-#define OUT_LIGHT CW_GPIOC->BSRR = GPIO_PIN_0
-// ABS 输出检测
-#define OUT_ABS CW_GPIOB->BSRR = GPIO_PIN_4
+#define ABS_PORT        CW_GPIOB
+#define ABS_PIN         GPIO_PIN_4
 
-// 一线通输出检测
-#define OUT_YXT CW_GPIOB->BSRR = GPIO_PIN_2
+#define ONEWIRE_PORT    CW_GPIOB
+#define ONEWIRE_PIN     GPIO_PIN_2
 
-// NFC输入检测
-#define IN_NFC ((CW_GPIOB->IDR & GPIO_PIN_5) == 0)
+#define NFC_PORT        CW_GPIOB
+#define NFC_PIN         GPIO_PIN_5
 
-// 485 控制使能
-#define EN485_TXD CW_GPIOA->BSRR = GPIO_PIN_8
-#define EN485_RXD CW_GPIOA->BRR = GPIO_PIN_8
+#define ACC_DET_PORT    CW_GPIOA
+#define ACC_DET_PIN     GPIO_PIN_7
 
-// 电门检测 输出
-#define IN_ACC_JC ((CW_GPIOA->IDR & GPIO_PIN_4) != 0)
-#define OUT_ACC   CW_GPIOC->BSRR = GPIO_PIN_4
+#define PC1_PORT        CW_GPIOC
+#define PC1_PIN         GPIO_PIN_1
 
-bool time_delay_check(uint8_t *lock, uint16_t *time_base, uint16_t deley_time);
-void usartInit(void);
-void send_c(void);
-uint8_t *alm_send(uint8_t len);
-void yxt_data_send(void);
-void DMA_INIT(void);
-void print_log(const char *sFormat, ...);
-void com_task_50us(void);
-void recv_dispose(void);
-void send_order(void);
-extern FILE_T file;
-extern uint8_t recv_over, time_out, NFC_read;
+#define EN485_TX_PORT   CW_GPIOA
+#define EN485_TX_PIN    GPIO_PIN_1
+
+#define EN485_RX_PORT   CW_GPIOB
+#define EN485_RX_PIN    GPIO_PIN_3
+
+void gpio_init(void);
+void light_task(void);
+void abs_task(void);
+void onewire_task(void);
+void nfc_task(void);
+void acc_task(void);
+
 #endif
--- user/timer.h
+++ user/timer.h
@@ -1,46 +1,8 @@
-#ifndef TIMER_C
-#define TIMER_C
+#ifndef TIMER_H
+#define TIMER_H
 #include "cw32f003.h"
-#include <stdint.h>
-#define TASK_10MS_TIME (10)
-#define TASK_50MS_TIME (50)
-#define TASK_100MS_TIME (100)
-#define TASK_500MS_TIME (500)
-#define TASK_1S_TIME (1000)
-#define clock_time_t    unsigned int
 
-#define CLOCK_S         (clock_time_t)(10000)
-#define CLOCK_MS        (clock_time_t)(1)
-
-uint32_t clock_time(void);
-void Clock_Time_Add(void);
-/**
- * A timer.
- *
- * This structure is used for declaring a timer. The timer must be set
- * with timer_set() before it can be used.
- *
- * \hideinitializer
- */
-struct timer 
-{
-	clock_time_t start;
-	clock_time_t interval;
-};
-typedef struct
-{
-    struct timer task_10ms;
-    struct timer task_50ms;
-    struct timer task_100ms;
-    struct timer task_1s;
-    struct timer task_500ms;
-} SystemTimer;
-extern SystemTimer system_timer;
-void timerinit(void);
-void timer_set(struct timer *t, clock_time_t interval);
-void timer_reset(struct timer *t);
-void timer_restart(struct timer *t);
-char timer_expired(struct timer *t);
-clock_time_t timer_GetCount(struct timer *t);
-void system_timer_init(void);
+void timer_init(void);
+uint32_t timer_ms(void);
+void delay_ms(uint32_t ms);
 #endif
